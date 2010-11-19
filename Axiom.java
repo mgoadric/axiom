d3a88067ceee73b01236655e66c753a83ffe8fbd
mgoadric <mgoadric@ca6c27c0-ee93-11df-8804-4d1ca90c4fa1>
2010-11-19 22:55:19 +0000

    first files

---
+++ Axiom.java
@@ -0,0 +1,33 @@
+/*
+ *      Axiom.java
+ *      
+ */
+
+
+public class Axiom {
+
+	// data members
+	
+	// constructor
+	
+	// methods
+
+    public static void main (String args[]) {
+    
+    // Initial Game State
+    Cube b1 = new Cube([-1, 0, 2], [1, 0, 0, 0, 3, 0]);
+    Cube b2 = new Cube([0, 0, 2], [0, 0, 0, 0, 0, 1]);
+    Cube b3 = new Cube([1, 0, 2], [1, 0, 0, 0, 3, 0]);
+    Cube b4 = new Cube([-1, 0, 2], [0, 1, 0, 0, 3, 0])
+    Cube b5 = new Cube([-1, 0, 2], [0, 1, 0, 0, 3, 0])
+    Cube b6 = new Cube([-1, 0, 2], [0, 1, 0, 0, 3, 0])
+    
+    Cube w1 = new Cube([-1, 0, 2], [0, 1, 0, 0, 3, 0])
+    Cube w2 = new Cube([-1, 0, 2], [0, 1, 0, 0, 3, 0])
+    Cube w3 = new Cube([-1, 0, 2], [0, 1, 0, 0, 3, 0])
+    Cube w4 = new Cube([-1, 0, 2], [0, 1, 0, 0, 3, 0])
+    Cube w5 = new Cube([-1, 0, 2], [0, 1, 0, 0, 3, 0])
+    Cube w6 = new Cube([-1, 0, 2], [0, 1, 0, 0, 3, 0])
+    
+    }
+}
