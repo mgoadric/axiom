17f2ef89979ced3ac70706c0f092fe8c8cd7b4ce
khardey <khardey@ca6c27c0-ee93-11df-8804-4d1ca90c4fa1>
2012-01-30 19:53:52 +0000

    More movetree work.

--- MonteCarloPlayer.java
+++ MonteCarloPlayer.java
@@ -6,20 +6,25 @@ public class MonteCarloPlayer extends Player {
 		
 		//number of times to run simulations till move choosen
     protected int simnum;
-    //Tree of moves
     protected MonteCarloTree moveTree;
-
     // Constructor    
-    public MonteCarloPlayer(int playerNum, int oppNum, int simnum, MonteCarloTree moveTree) {
+    public MonteCarloPlayer(int playerNum, int oppNum, int simnum) {
         super(playerNum, oppNum);
         this.simnum = simnum;
-        this.moveTree = moveTree;
+        this.moveTree = new MonteCarloTree(0, 0, 10);
     }
     
     public int chooseMove(BoardGame board) {
         // Returns the next move that this player wants to make
         int move = -1;
-       // move = monteCarloMove( board, ply);
+        move = monteCarloMove(board);
         return move;
+    }
+    
+    public int monteCarloMove(BoardGame board){
+    		for(int i =0; i < simnum; i++){
+    				moveTree.findMove(board);
+    		}
+    		return moveTree.getBestMove();
     }
 } 
--- MonteCarloTree.java
+++ MonteCarloTree.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class MonteCarloTree {
 		
 		private Node root;
+		private int visitLimit;
 
 		private class Node {
 				private int wins;
@@ -14,11 +15,26 @@ public class MonteCarloTree {
 				private List<Node> children;
 		}
 		
-		public MonteCarloTree(int wins, int visits) {
+		public MonteCarloTree(int wins, int visits, int visitLimit) {
+				this.visitLimit = visitLimit;
 				root = new Node();
 				root.wins = wins;
 				root.visits = visits;
 				root.children = new ArrayList<Node>();
+		}
+		
+		public void findMove(Gameboard board){
+			//root node empty, generate children based on legalmoves. Need to bring in player. Will also need opponent player to play randomly against.
+			//Choose node, has no children, hasn't reached visitLimit, then random game using that move M.
+			//Make move M. For legal moves of Random Opponent, choose one. Generate legalmoves for Me at that move, etc.
+			//Update visits to node on game completion. When gameOver, update wins if won for that player.
+			//How to choose moves which are doing well? Can't continuously pick random moves to make, want to research moves doing well.
+			//On visitLimit reached, generate children for that move. 
+		}
+		
+		public int getBestMove(){
+				//return best move found in findMove. Move with most wins?, some kind of other metric
+			return -1;	
 		}
 }
 
