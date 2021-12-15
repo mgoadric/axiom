b678cf1283d2c9ffcda1fc1b5edac40a9510abf7
Mark Goadrich <goadrich@hendrix.edu>
2021-12-15 13:15:04 -0600

    Starting C# version

---
+++ csharp/Axiom/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Axiom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
